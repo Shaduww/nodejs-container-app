37eee29b927fa85a0fd05c81d1e5d21b61179c61
Youssef Abdelhamid <yorock45@hotmail.com>
2025-04-10 09:29:11 -0400

    Trigger CICD and add my name to app

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Dockerized Node.js App!');
+  res.send('Hello my name is Youssef Abdelhamid, Dockerized Node.js App!');
 });
 
 app.listen(PORT, () => {
